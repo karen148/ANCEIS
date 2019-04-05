395e4a7d7597a7adf50a1386af83a162b56325cc
Karen <karen_vivianasis@hotmail.com>
2019-04-04 22:44:52 -0500

    Update WriterObject.java

--- WriterObject.java
+++ WriterObject.java
@@ -36,39 +36,27 @@ public class WriterObject
    
    private static void guardarAfiliado(Afiliaciones afiliaciones){
       try {
-           FileOutputStream fos = new FileOutputStream( "afiliaciones.obj", false );
-           do{
-               try{
-                   ObjectOutputStream oos = new ObjectOutputStream( fos );
-                   oos.writeObject(afiliaciones);   
-                   oos.close();
-               }
-               catch(Exception e){
-                  System.out.println("El registro de afiliaciones no se ha completado"); 
-               }
-            }while(false);
+           FileOutputStream fos = new FileOutputStream("afiliaciones.obj",false);
+           ObjectOutputStream oos = new ObjectOutputStream( fos );
+           oos.writeObject(afiliaciones); 
+           System.out.println("Exito writing");
+           oos.close();
        }
        catch(Exception e){
-           e.printStackTrace();
+           System.out.println("El registro de afiliaciones no se ha completado");
        } 
    }
    
    private static void guardarLireria(Libreria libreria){
       try {
-           FileOutputStream fos = new FileOutputStream( "libreria.obj", false );
-           do{
-               try{
-                   ObjectOutputStream oos = new ObjectOutputStream( fos );
-                   oos.writeObject(libreria);   
-                   oos.close();
-               }
-               catch(Exception e){
-                   System.out.println("El registro de cafeteria no se ha completado");
-               }
-            }while(false);
+           FileOutputStream fos = new FileOutputStream("libreria.obj",false);
+           ObjectOutputStream oos = new ObjectOutputStream( fos );
+           oos.writeObject(libreria); 
+           System.out.println("Exito writing");
+           oos.close();
        }
        catch(Exception e){
-           e.printStackTrace();
-       } 
+           System.out.println("El registro de libreria no se ha completado");
+       }
    }
 }
